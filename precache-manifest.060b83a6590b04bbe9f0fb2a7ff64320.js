3262845c19e4e218c6ff91a2a8e6a703ae882dc2
phiratio <phiratio161@gmail.com>
2020-03-28 02:23:44 +0200

    Updates

---
+++ precache-manifest.060b83a6590b04bbe9f0fb2a7ff64320.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747c9f88d40a9d3b1d557447d96c2c58",
+    "url": "/bots/index.html"
+  },
+  {
+    "revision": "3928d152d61a615eec21",
+    "url": "/bots/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "4f3e0ec7d18da2ef904a",
+    "url": "/bots/static/css/main.3ab8f593.chunk.css"
+  },
+  {
+    "revision": "3928d152d61a615eec21",
+    "url": "/bots/static/js/2.8db9c0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bots/static/js/2.8db9c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3e0ec7d18da2ef904a",
+    "url": "/bots/static/js/main.3b268fbc.chunk.js"
+  },
+  {
+    "revision": "fe138cb66a9a5606faeb",
+    "url": "/bots/static/js/runtime-main.f76c9064.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/bots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
