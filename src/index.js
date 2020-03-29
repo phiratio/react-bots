fc1b7cf9cdde9e2eb052a73c79332e7009f05b9b
phiratio <phiratio161@gmail.com>
2020-03-30 01:43:15 +0300

    logger added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider, connect} from 'react-redux';
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import {createLogger} from "redux-logger/src";
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import {searchRobots} from "./reducers";
+const logger = createLogger();
 // action -> reducer -> store -> changes | uses flux pattern
-const store = createStore(searchRobots)
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}>
                     <App />
