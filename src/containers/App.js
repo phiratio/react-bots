143ebbee5face1cb9a8ee615d6abbac2b0a682a9
phiratio <phiratio161@gmail.com>
2020-03-29 22:57:26 +0300

    redux connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from "../components/ErrorBoundry";
 import './App.css';
+import {setSearchField} from "../actions";
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+};
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
 
@@ -22,21 +35,19 @@ class App extends Component {
             });
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    };
 
     render() {
-        const {robots, searchfield} = this.state;
+        const {robots} = this.state;
+        const {searchField, onSearchChange} = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
         return !robots.length ?
             <h1>Loading</h1> :
             (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={filteredRobots}/>
@@ -47,4 +58,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers.js
+++ src/reducers.js
@@ -7,7 +7,8 @@ const initialState = {
 export const searchRobots = (state = initialState, action = {}) => {
     switch (action.type) {
         case CHANGE_SEARCHFIELD:
-            return {...state, searchField: action.payload};
+            // return {...state, searchField: action.payload};
+            return Object.assign({},state,{searchField: action.payload})
         default:
             return state;
     }
